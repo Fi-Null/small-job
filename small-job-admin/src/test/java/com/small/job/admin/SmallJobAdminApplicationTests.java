6ff3015ec79c7a93be3f5fada54bb84146ba716f
向科 <xiangke6@jd.com>
2019-12-26 21:06:44 +0800

    init2

---
+++ small-job-admin/src/test/java/com/small/job/admin/SmallJobAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.small.job.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallJobAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
